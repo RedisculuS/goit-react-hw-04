407e7039e9b9a93049e3a61ec826db8533d4b3e2
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2024-07-19 09:30:12 +0300

    4th commit
    Modal fixed + description added

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -12,13 +12,25 @@ export const ImageModal = ({ isOpen, onRequestClose, image }) => {
       overlayClassName={css.modalOverlay}
       className={css.modalContent}
     >
-     
       {image && (
-        <img
-          src={image.urls.full}
-          alt={image.description}
-          className={css.imageModal}
-        />
+        <div className={css.imageContainer}>
+          <img
+            src={image.urls.full}
+            alt={image.description}
+            className={css.imageModal}
+          />
+          <div className={css.imageInfo}>
+            <p>
+               {image.user.name}
+            </p>
+            <p>
+              "{image.description || "No description available"}""
+            </p>
+            <p>
+              Likes: {image.likes}
+            </p>
+          </div>
+        </div>
       )}
     </Modal>
   );
